cbd70788c94621cadc3c168295a34b4bcacf2dac
Jakub Wąsik <eghizio@gmail.com>
2019-04-16 03:15:37 +0200

    Basic server and API

--- server/server.js
+++ server/server.js
@@ -14,30 +14,64 @@ mongoose
 const db = mongoose.connections;
 
 /*********************///wil be moved to router dir WIP
-app.get("/",(req, res) => {
+//Query to lower middleware
+app.use((req, res, next) => {
+    for(let key in req.query)
+        req.query[key] = req.query[key].toLowerCase(); 
+    //crashes at multiple name values ex: plants/q?name=CEBULE&name=marchew
+    next();
+});
+
+app.get("/", (req, res) => {
     res.type("text/html"); //will serve a menu page with all endpoints or smthin 
     res.send("/" +"</br>"+"/plants"+"</br>"+"/plants/q?parameter=value");
 });
 
-app.get("/plants",(req, res) => {
+app.get("/plants", (req, res) => {
     res.json(plants_data);
 });
 
+app.get("/plant/:id", (req, res) => {
+    res.json(plants_data.filter(plant => plant.id == req.params.id)[0]);
+});
+
+
+//how to get just objects list not an array :c
 //test route, get single plant ex /plants/q?id=6
-//for now hardcoded. returns the first matching parameter(id>name>time>etc) 
-app.get("/plants/q",(req, res) => {
-    let querriedPlant = plants_data.filter(plant =>
-        plant.id == req.query.id || 
-        plant.name.toLowerCase() == req.query.name.toLowerCase() ||
-        plant.time == req.query.time ||
-        plant.crop == req.query.crop ||
-        plant.sx == req.query.sx ||
-        plant.sy == req.query.sy ||
-        plant.level == req.query.level
-    )[0]; //remove [0] to get all matching plants
-    res.json(querriedPlant);
+//for now hardcoded. returns all matching parameters 
+app.get("/plants/q", (req, res) => {
+    const { query } = req;
+
+    let querriedPlants = plants_data.filter(plant =>
+        plant.id == query.id || 
+        plant.name.toLowerCase() == query.name ||
+        plant.time == query.time ||
+        plant.crop == query.crop ||
+        plant.sx == query.sx ||
+        plant.sy == query.sy ||
+        plant.level == query.level
+    ); //returns array ;/
+    res.json(querriedPlants.length === 1 ? querriedPlants[0]:querriedPlants);
     // for(const key in req.query) make it universal WIP
     // console.log(`${key} = ${req.query[key]}`);
+});
+
+//maybe just change to /plant?parameter=value
+//returns first matching parameter(id>name>time>etc) 
+app.get("/plant/q", (req, res) => {
+    const { query } = req;
+
+    let querriedPlant = plants_data.filter(plant =>
+        plant.id == query.id || 
+        plant.name.toLowerCase() == query.name ||
+        plant.time == query.time ||
+        plant.crop == query.crop ||
+        plant.sx == query.sx ||
+        plant.sy == query.sy ||
+        plant.level == query.level
+    )[0]; //remove [0] to get all matching plants tho it will be an array
+
+    res.json(querriedPlant); // omg cries in DRY xD
 });
 /*********************/
 
