aa998d9249c6404b9f40f9e84afe042595550399
Jakub Wąsik <eghizio@gmail.com>
2019-04-20 21:02:08 +0200

    Plants API done, complete queries and filters, middleware makeover, merging plant routes

--- server/middleware/queryParser.js
+++ server/middleware/queryParser.js
@@ -1,8 +1,6 @@
 const escapeDiacritics = require("./diacritics");
 
 const queryParser = (req, res, next) => {
-    console.log("p:", req.params);
-    console.log("q:", req.query);
     
     if(req.params.name)
         req.params.name = escapeDiacritics(req.params.name.toLowerCase());
@@ -13,9 +11,6 @@ const queryParser = (req, res, next) => {
         else
             req.query.name = escapeDiacritics( req.query.name.toLowerCase() );
     }
-
-    console.log("p:", req.params);
-    console.log("q:", req.query);
 
     next();
 };
